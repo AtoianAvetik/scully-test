6bf1944469211c2e8ab9e1281a8a8aae5681655b
AtoianAvetik <18209493+AtoianAvetik@users.noreply.github.com>
2021-08-16 15:13:16 +0300

    - setup netlify admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
     {path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
     {path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule)},
+    {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable } from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
@@ -8,7 +11,16 @@ import { ScullyRoutesService } from '@scullyio/ng-lib';
 })
 export class AppComponent {
     title = 'scully-test';
+    isAdminPage$: Observable<boolean> = this.router.events.pipe(
+        filter((evt: any) => {
+            return evt instanceof NavigationEnd;
+        }),
+        map((evt: NavigationEnd) => {
+            return evt.url.includes('/admin');
+        }),
+    );
 
-    constructor(public scully:ScullyRoutesService) {
+    constructor(public scully:ScullyRoutesService,
+                private router: Router) {
     }
 }
